a5d08e52709c5f0ad09580c420b692923334c386
jonashw <2jonwilson@gmail.com>
2018-07-20 21:56:31 -0700

    Bezier.js: minor rename for improved clarity

--- src/Bezier.js
+++ src/Bezier.js
@@ -4,7 +4,7 @@ var Bezier = (() => {
     ** we avoid creating and returning an object in a tight loop. 
     ** Read: memory leak. */
     //Reference: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Specific_cases
-    function q(t,p0,p1,p2,callback){
+    function quadraticEase(t,p0,p1,p2,callback){
         let t2 = t*t;
         let p0c = 1-2*t+t2;
         let p1c = 2*(t-t2);
@@ -13,7 +13,7 @@ var Bezier = (() => {
         let y = (p0c * p0.y) + (p1c * p1.y) + (p2c * p2.y);
         callback(x,y);
     }
-    function c(t,p0,p1,p2,p3,callback){
+    function cubicEase(t,p0,p1,p2,p3,callback){
         let t2 = t*t;
         let t3 = t*t*t;
         let p0c = (1 - 3*t + 3*t2 - t3);
@@ -29,16 +29,16 @@ var Bezier = (() => {
         linear: (p0,p1) => {
             line(p0.x, p0.y, p1.x, p1.y);
         },
-        quadraticEase: q,
+        quadraticEase: quadraticEase,
         quadratic: function(p0,p1,p2,callback) {
             for(var t=0; t<1; t+=Bezier.step){
-                q(t,p0,p1,p2,callback);
+                quadraticEase(t,p0,p1,p2,callback);
             }
         },
-        cubicEase: c,
+        cubicEase: cubicEase,
         cubic: function(p0,p1,p2,p3,callback) {
             for(var t=0; t<1; t+=Bezier.step){
-                c(t,p0,p1,p2,p3,callback);
+                cubicEase(t,p0,p1,p2,p3,callback);
             }
         }
     };
